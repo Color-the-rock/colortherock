fc89e081bee6c87cf6122d70b03ac6ae7c388de5
shyunju7 <shyunju7@naver.com>
2023-01-17 14:12:16 +0900

    ✨ feat : AppRouter 설정

--- frontend/color-the-rock-app/src/AppRouter.js
+++ frontend/color-the-rock-app/src/AppRouter.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Intro from "./pages/Intro";
+import Login from "./pages/Login";
+import Streaming from "./pages/Streaming";
+import Board from "./pages/Board";
 const AppRouter = () => {
+  // 로그인 여부에 따라 router 설정
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Intro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/streaming" element={<Streaming />} />
+        <Route path="/board" element={<Board />} />
       </Routes>
     </Router>
   );
