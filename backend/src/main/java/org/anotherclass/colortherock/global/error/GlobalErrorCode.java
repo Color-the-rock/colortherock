9b8202c65cd6e8fcd13172ece259d9cfef8f9034
johan <johankkong@gmail.com>
2023-01-18 11:43:35 +0900

    :zap: update : add errorcode and update exception

--- backend/src/main/java/org/anotherclass/colortherock/global/error/GlobalErrorCode.java
+++ backend/src/main/java/org/anotherclass/colortherock/global/error/GlobalErrorCode.java
@@ -19,7 +19,8 @@ public enum GlobalErrorCode {
     NOT_VIDEO_OWNER(400, "V200", "영상의 주인과 요청한 멤버가 다릅니다."),
     // 멤버와 관련된 Exception
     DUPLICATE_NICKNAME(400, "M100", "중복 닉네임 입니다"),
-    ;
+
+    MALFORMED_DATE(400, "R100", "잘못된 날짜 형식입니다.");
 
     private final String code;
     private final String message;
