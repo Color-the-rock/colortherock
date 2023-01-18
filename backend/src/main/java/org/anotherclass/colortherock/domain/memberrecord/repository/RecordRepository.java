38c4cb55c00401b60f05ad10fde011cfb7af2835
johan <johankkong@gmail.com>
2023-01-18 10:04:11 +0900

    :sparkles: feat : add recordsByColor feature in MemberRecords

--- backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/repository/RecordRepository.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/repository/RecordRepository.java
@@ -1,11 +1,10 @@
 package org.anotherclass.colortherock.domain.memberrecord.repository;
 
 import org.anotherclass.colortherock.domain.member.entity.Member;
-import org.anotherclass.colortherock.domain.memberrecord.entity.MemberRecord;
+import org.anotherclass.colortherock.domain.video.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RecordRepository extends JpaRepository<MemberRecord, Long> {
-    MemberRecord findByMember(Member member);
+public interface RecordRepository extends JpaRepository<Video, Long> {
+    Long countByMemberAndLevel(Member member, int level);
+    Long countByMemberAndLevelAndIsSuccessIsTrue(Member member, int level);
 }
