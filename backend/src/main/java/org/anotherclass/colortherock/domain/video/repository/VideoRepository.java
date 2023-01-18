7160f188e8559aac5c883fd3a45b548f62b56601
johan <johankkong@gmail.com>
2023-01-18 12:25:16 +0900

    :sparkles: feat : add totalStat feature

---
+++ backend/src/main/java/org/anotherclass/colortherock/domain/video/repository/VideoRepository.java
@@ -0,0 +1,15 @@
+package org.anotherclass.colortherock.domain.video.repository;
+
+import org.anotherclass.colortherock.domain.member.entity.Member;
+import org.anotherclass.colortherock.domain.video.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Long> {
+    List<Video> findAllByMember(Member member);
+    List<Video> findAllByMemberAndShootingDate(Member member, LocalDate date);
+}
