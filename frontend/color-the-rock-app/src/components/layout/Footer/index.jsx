6239442d466eb0de8c77a7a1a15f100b9ef1df5c
shyunju7 <shyunju7@naver.com>
2023-01-18 15:14:52 +0900

    🎨 style : Footer 크기 지정 및 파일분리

---
+++ frontend/color-the-rock-app/src/components/layout/Footer/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./style";
+const Footer = () => {
+  return <S.FooterContainer></S.FooterContainer>;
+};
+export default Footer;
---
+++ frontend/color-the-rock-app/src/components/layout/Footer/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 216px;
+  background-color: var(--color-background);
+`;
