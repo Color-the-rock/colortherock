9239210e209568aff5497df25425842c70a88053
johan <johankkong@gmail.com>
2023-01-18 10:04:11 +0900

    :sparkles: feat : add recordsByColor feature in MemberRecords

---
+++ backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/response/StatisticsDTO.java
@@ -0,0 +1,10 @@
+package org.anotherclass.colortherock.domain.memberrecord.response;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class StatisticsDTO {
+    int level;
+    long total;
+    long success;
+}
