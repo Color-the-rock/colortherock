4b9489881eaea64ec51f6e945babb21c6597bce5
yezi <yejixheo@gmail.com>
2023-01-19 16:17:24 +0900

    ⚡ update : change response object (ResponseEntity to BaseResponse)

--- backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/controller/VideoBoardController.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/controller/VideoBoardController.java
@@ -5,30 +5,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.anotherclass.colortherock.domain.videoboard.request.VideoBoardSearchRequest;
 import org.anotherclass.colortherock.domain.videoboard.response.VideoBoardSummaryResponse;
 import org.anotherclass.colortherock.domain.videoboard.service.VideoBoardService;
+import org.anotherclass.colortherock.global.common.BaseResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @Slf4j
 @RequiredArgsConstructor
-@RestController
 @RequestMapping("/api/video")
 public class VideoBoardController {
 
     private final VideoBoardService videoBoardService;
 
     @GetMapping("/board")
-    public ResponseEntity<List<VideoBoardSummaryResponse>> getVideoTemp
-            (@PageableDefault(size=15, sort="id", direction = Sort.Direction.DESC) Pageable pageable, VideoBoardSearchRequest cond) {
-        System.out.println(cond.toString());
-        return ResponseEntity.ok()
-                .body(videoBoardService.getSuccessVideos(cond, pageable));
+    public BaseResponse<List<VideoBoardSummaryResponse>> getVideoTemp
+            (@PageableDefault(size=16, sort="id", direction = Sort.Direction.DESC) Pageable pageable, VideoBoardSearchRequest cond) {
+        List<VideoBoardSummaryResponse> successVideoList = videoBoardService.getSuccessVideos(cond, pageable);
+        return new BaseResponse<>(successVideoList);
     }
 
 
