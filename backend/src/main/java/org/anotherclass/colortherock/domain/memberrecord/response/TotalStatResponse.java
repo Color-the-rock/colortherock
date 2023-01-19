014c8422937912f120d174efcd215e568ac7ec54
johan <johankkong@gmail.com>
2023-01-19 10:07:59 +0900

    :art: style : change DTO name to Response

--- backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/response/TotalStatResponse.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/response/TotalStatResponse.java
@@ -3,7 +3,7 @@ package org.anotherclass.colortherock.domain.memberrecord.response;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class TotalStatDTO {
+public class TotalStatResponse {
     int videoCount;
     int videoLengthSum;
     int successCount;
