d93eac0939f40b33c446130bf9622cb22b2900ec
yezi <yejixheo@gmail.com>
2023-01-19 16:18:21 +0900

    🎨 style : delete ToString annotation

--- backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/request/VideoBoardSearchRequest.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/request/VideoBoardSearchRequest.java
@@ -2,11 +2,9 @@ package org.anotherclass.colortherock.domain.videoboard.request;
 
 import lombok.Data;
 import lombok.Getter;
-import lombok.ToString;
 
 @Data
 @Getter
-@ToString
 public class VideoBoardSearchRequest {
     private Long storeId;
     private String color;
