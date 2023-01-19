fcfce74149b3b5e5aef7b5f047a7790bec71e254
yezi <yejixheo@gmail.com>
2023-01-19 11:15:35 +0900

    🎨 style : rename video board dto

--- backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/response/VideoBoardSummaryResponse.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/response/VideoBoardSummaryResponse.java
@@ -3,21 +3,18 @@ package org.anotherclass.colortherock.domain.videoboard.response;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Builder
 @Data
-public class VideoBoardSummaryDto {
-    @NotNull
+public class VideoBoardSummaryResponse {
+
     private Long videoBoardId;
-    @NotBlank
+
     private String title;
-    @NotBlank
     private String thumbnailURL;
-    @NotBlank
+
     private String color;
-    @NotNull
+
     private LocalDateTime writtenTime;
 }
