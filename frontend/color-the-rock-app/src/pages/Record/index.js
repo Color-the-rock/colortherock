1a82b2e9a3028bcab91221d59c5d92e916f06288
shyunju7 <shyunju7@naver.com>
2023-01-19 10:36:35 +0900

    🎨 style : Style Record Page(Title, SubTitle, Text, GradientText)

--- frontend/color-the-rock-app/src/pages/Record/index.js
+++ frontend/color-the-rock-app/src/pages/Record/index.js
@@ -1,6 +1,21 @@
 import React from "react";
-
+import * as S from "./style";
 const Record = () => {
-  return <div> record page</div>;
+  return (
+    <S.Container>
+      <S.Title>나의 운동 기록</S.Title>
+      <S.TextWrapper>
+        <S.Text>
+          예지님은 <S.GradientText>30일</S.GradientText>동안
+        </S.Text>
+        <S.Text>
+          총 <S.GradientText>100개의 문제</S.GradientText>에 성공했어요!
+        </S.Text>
+      </S.TextWrapper>
+      {/* 누적기록 */}
+      {/* 일자별 기록 */}
+      {/* 영상목록 */}
+    </S.Container>
+  );
 };
 export default Record;
---
+++ frontend/color-the-rock-app/src/pages/Record/style.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
+  padding: 0px 16px;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 2.125rem;
+  letter-spacing: -0.01em;
+`;
+
+export const TextWrapper = styled.div`
+  margin: 32px 0px;
+`;
+
+export const Text = styled.p`
+  width: 100%;
+  text-align: center;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  letter-spacing: -0.02em;
+`;
+
+export const GradientText = styled.label`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  letter-spacing: -0.02em;
+  background: linear-gradient(
+    135deg,
+    var(--color-brand-gradient-start) 0%,
+    var(--color-brand-gradient-end) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+`;
+
+export const SubTitle = styled.h2`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 0.875rem;
+  line-height: 1.5rem;
+  /* or 171% */
+
+  letter-spacing: -0.01em;
+`;
