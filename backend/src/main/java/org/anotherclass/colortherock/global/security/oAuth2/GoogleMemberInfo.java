158001efa58bce47ca3c44692821f770e18eba4d
Tae gyu Choi <suker800@gmail.com>
2023-01-19 14:59:58 +0900

    :sparkles: feat : 소셜 로그인 기능 구현
    구글 로그인 기능 구현
    카카오 로그인 기능 구현

---
+++ backend/src/main/java/org/anotherclass/colortherock/global/security/oAuth2/GoogleMemberInfo.java
@@ -0,0 +1,21 @@
+package org.anotherclass.colortherock.global.security.oAuth2;
+
+import org.anotherclass.colortherock.domain.member.entity.Member.RegistrationId;
+
+public class GoogleMemberInfo implements MemberInfo {
+    private final String email;
+
+    public GoogleMemberInfo(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public RegistrationId getRegistrationId() {
+        return RegistrationId.google;
+    }
+}
