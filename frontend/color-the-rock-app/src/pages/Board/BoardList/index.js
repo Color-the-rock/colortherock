8936da062fec8ff6add43b773dab1a369bd3a7cd
shyunju7 <shyunju7@naver.com>
2023-01-20 15:29:40 +0900

    :bug: fix : boardList.jsx 수정
    import 오류 해결 및 함수 생성

--- frontend/color-the-rock-app/src/pages/Board/BoardList/index.js
+++ frontend/color-the-rock-app/src/pages/Board/BoardList/index.js
@@ -1,36 +1,34 @@
-import { Mobile, Desktop } from "../../../components/layout/Template"
+import { Mobile, Desktop } from "../../../components/layout/Template";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { useInfiniteScroll } from "../../../hooks/useInfiniteScroll"
+import useInfiniteScroll from "../../../hooks/useInfiniteScroll";
 
-
-import Video from "../../../components/Board/BoardList"
+import Video from "../../../components/Board/BoardList";
 
 import * as S from "./style";
 import axios from "axios";
 
 // import {defaultInstance, authApi} from "../../api/utils/index";
 const Board = () => {
-
   const [isFetching, setIsFetching] = useInfiniteScroll(updataFunctionOnScroll);
 
   const [videoData, setVideoData] = useState([]);
   const [searchValue, setSearchValue] = useState("");
-  
-  
+
   const navigate = useNavigate();
 
-  
   useEffect(() => {
     fetchData();
-  }, [])
-  
+  }, []);
+
   // 영상데이터 불러오기
   const fetchData = async () => {
     console.log("영상 불러오기");
-    const res = await axios.get("https://api.themoviedb.org/3//movie/top_rated?api_key=c4c61062f0586b0c14f0df48b5eda1f1");
-    console.log("data",  res);
+    const res = await axios.get(
+      "https://api.themoviedb.org/3//movie/top_rated?api_key=c4c61062f0586b0c14f0df48b5eda1f1"
+    );
+    console.log("data", res);
     setVideoData(res.data.results);
     // const possible = await authApi.get(" ");
     // if(possible) {
@@ -39,62 +37,50 @@ const Board = () => {
     // } else {
 
     // }
-  }
+  };
+  const fetchFunction = () => {};
   const updataFunctionOnScroll = async () => {
     try {
       const res = await fetchFunction();
       setVideoData((prev) => [res, ...prev]);
-    } catch(error) {
-      
+    } catch (error) {
     } finally {
       setIsFetching(false);
     }
-  }
-  }
+  };
 
   const handleChange = (e) => {
     setSearchValue(e.target.value);
-  }
+  };
 
   return (
-  <div>
-    <Desktop>
-      <S.Container>
-
-      </S.Container>
-    </Desktop>
-    <Mobile>
-      <S.Container>
-        <h2>완등 영상 보기</h2>
-        <hr/>
-        <div>보고 싶은 레벨과 색상을 입력하세요.</div>
-        <input
-          value={searchValue}
-          onChange={handleChange}
-          type="text"
-          placeholder="암장 입력."
-        >
-        </input>
-        {
-          videoData.length > 0 &&
-          <S.VideoWrap>
-            {
-              videoData.map((data, index) => (
-                <Video 
-                  key={index}
-                  data={data}
-                />
-                
-              ))
-            }
-          </S.VideoWrap>
-        }
-      </S.Container>
-    </Mobile>
-  </div>
+    <div>
+      <Desktop>
+        <S.Container></S.Container>
+      </Desktop>
+      <Mobile>
+        <S.Container>
+          <h2>완등 영상 보기</h2>
+          <hr />
+          <div>보고 싶은 레벨과 색상을 입력하세요.</div>
+          <input
+            value={searchValue}
+            onChange={handleChange}
+            type="text"
+            placeholder="암장 입력."
+          ></input>
+          {videoData.length > 0 && (
+            <S.VideoWrap>
+              {videoData.map((data, index) => (
+                <Video key={index} data={data} />
+              ))}
+            </S.VideoWrap>
+          )}
+        </S.Container>
+      </Mobile>
+    </div>
   );
 };
 export default Board;
 
-
-// 무한스크롤 시 마지막 video id를 넘겨준다. 
+// 무한스크롤 시 마지막 video id를 넘겨준다.
