df8e472af78994b61e67711d14250045b225effb
Choi Tae gyu <suker800@gmail.com>
2023-01-23 18:15:39 +0900

    :sparkles: Live room 생성 API 구현
    - response로 어떠한 값이 필요 한지 불명

--- backend/src/main/java/org/anotherclass/colortherock/domain/live/cotroller/LiveController.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/live/cotroller/LiveController.java
@@ -1,12 +1,28 @@
 package org.anotherclass.colortherock.domain.live.cotroller;
 
 import lombok.RequiredArgsConstructor;
+import org.anotherclass.colortherock.domain.live.request.CreateLiveRequest;
 import org.anotherclass.colortherock.domain.live.service.LiveService;
+import org.anotherclass.colortherock.domain.member.entity.MemberDetails;
+import org.anotherclass.colortherock.global.common.BaseResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
+
 public class LiveController {
 
     private final LiveService liveService;
+
+    @GetMapping("/live")
+    public BaseResponse<?> createLive(@AuthenticationPrincipal MemberDetails memberDetails, @RequestBody CreateLiveRequest request) {
+        liveService.createLiveRoom(memberDetails, request);
+
+        return new BaseResponse<>();
+    }
 }
--- backend/src/main/java/org/anotherclass/colortherock/domain/live/service/LiveService.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/live/service/LiveService.java
@@ -1,13 +1,52 @@
 package org.anotherclass.colortherock.domain.live.service;
 
-import lombok.RequiredArgsConstructor;
+import io.openvidu.java.client.*;
+import org.anotherclass.colortherock.domain.live.entity.Live;
 import org.anotherclass.colortherock.domain.live.repository.LiveRepository;
+import org.anotherclass.colortherock.domain.live.request.CreateLiveRequest;
+import org.anotherclass.colortherock.domain.member.entity.Member;
+import org.anotherclass.colortherock.domain.member.entity.MemberDetails;
+import org.anotherclass.colortherock.domain.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class LiveService {
 
     private final LiveRepository liveRepository;
+    private final MemberRepository memberRepository;
 
+    private final OpenVidu openVidu;
+
+    public LiveService(LiveRepository liveRepository,
+                       MemberRepository memberRepository,
+                       @Value("${OPENVIDU_URL}") String OPENVIDU_URL,
+                       @Value("${OPENVIDU_SECRET}") String OPENVIDU_SECRET) {
+        this.liveRepository = liveRepository;
+        this.memberRepository = memberRepository;
+        this.openVidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+    public void createLiveRoom(MemberDetails memberDetails, CreateLiveRequest request) {
+        Long id = memberDetails.getMember().getId();
+        Member member = memberRepository.findById(id).orElseThrow();
+        Session session;
+        // TODO 어떤 오류가 나는지 불명
+        try {
+            session = openVidu.createSession();
+        } catch (OpenViduJavaClientException | OpenViduHttpException e) {
+            throw new RuntimeException(e);
+        }
+        String sessionId = session.getSessionId();
+
+        // TODO 섬네일 어떻게 받을지?
+        Live live = request.toEntity(sessionId, "대충 섬네일", member);
+        Live save = liveRepository.save(live);
+        try {
+            Connection connection = session.createConnection(new ConnectionProperties.Builder().role(OpenViduRole.PUBLISHER).build());
+            String connectionId = connection.getConnectionId();
+        } catch (OpenViduJavaClientException | OpenViduHttpException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
