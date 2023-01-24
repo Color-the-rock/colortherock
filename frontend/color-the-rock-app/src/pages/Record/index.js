c303662066ae83a7a66346fef2d880db9f018c72
송현주 <songthdo427@gmail.com>
2023-01-24 18:01:06 +0900

    :lipstick: style :
    add calendar style & Record Page UI 수정

--- frontend/color-the-rock-app/src/pages/Record/index.js
+++ frontend/color-the-rock-app/src/pages/Record/index.js
@@ -1,29 +1,39 @@
-import React from "react";
-import { DailyRecordWrapper } from "../../components/Record/DailyRecord/style";
+import React, { useState } from "react";
 import StackedGraph from "../../components/Record/StackedGraph";
+import "react-calendar/dist/Calendar.css";
+import "./calendar.css";
+
 import * as S from "./style";
 const Record = () => {
+  const [value, onChange] = useState(new Date());
+
   return (
     <S.Container>
       <S.Title>나의 운동 기록</S.Title>
       {/* 모바일 버전 */}
       <S.TextWrapper>
         <S.Text>
-          예지님은 <S.GradientText>30일</S.GradientText>동안
+          나싸피님은 <S.GradientText>30일</S.GradientText>동안
         </S.Text>
         <S.Text>
           총 <S.GradientText>100개의 문제</S.GradientText>에 성공했어요!
         </S.Text>
       </S.TextWrapper>
-      {/* 누적기록 */}
-      <S.SubTitle>누적 기록</S.SubTitle>
-      <StackedGraph />
-      {/* 일자별 기록 */}
-      <S.SubTitle>일자별 기록</S.SubTitle>
-      <DailyRecordWrapper />
-      {/* 영상목록 */}
+      {/* 레벨별 도전 현황 */}
+      <S.SubTitle>레벨별 도전 현황</S.SubTitle>
+      <S.GraphWrapper>
+        <StackedGraph />
+      </S.GraphWrapper>
+      {/* 활동 통계 */}
+      <S.SubTitle>활동 통계</S.SubTitle>
+      <S.GraphWrapper></S.GraphWrapper>
+      {/* <StatisticGraph /> */}
+      <S.SubTitle>일별 도전 기록</S.SubTitle>
+      <S.RecordCalendar onChange={onChange} value={value} />
 
-      {/* 데스크탑 버전 */}
+      <S.SubTitle>성공 영상</S.SubTitle>
+
+      <S.SubTitle>실패 영상</S.SubTitle>
     </S.Container>
   );
 };
