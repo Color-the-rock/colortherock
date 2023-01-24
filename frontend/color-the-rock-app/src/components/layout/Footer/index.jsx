e48f12cf423db99ee3fc4851d07fa6cc51f49cf2
송현주 <songthdo427@gmail.com>
2023-01-24 13:05:27 +0900

    :lipstick: style :
    footer UI 추가

--- frontend/color-the-rock-app/src/components/layout/Footer/index.jsx
+++ frontend/color-the-rock-app/src/components/layout/Footer/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import * as S from "./style";
 const Footer = () => {
-  return <S.FooterContainer></S.FooterContainer>;
+  return (
+    <S.FooterContainer>
+      <S.CopyRight>©2023</S.CopyRight>
+    </S.FooterContainer>
+  );
 };
 export default Footer;
