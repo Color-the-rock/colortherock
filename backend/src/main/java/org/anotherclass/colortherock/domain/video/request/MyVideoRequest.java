62a89470bd38c51cfcde89d4d3a14d0d5fd56c11
johan <johankkong@gmail.com>
2023-01-25 12:10:39 +0900

    :zap: update : update success videos with no-offset

--- backend/src/main/java/org/anotherclass/colortherock/domain/video/request/MyVideoRequest.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/video/request/MyVideoRequest.java
@@ -1,17 +1,25 @@
 package org.anotherclass.colortherock.domain.video.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.anotherclass.colortherock.domain.member.entity.Member;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MyVideoRequest {
+    @NotNull @Positive
     private Long videoId;
+    @NotNull @DateTimeFormat(pattern = "YYYY-MM-DD")
     private LocalDate shootingDate;
     private Member member;
+    @NotNull
     private boolean isSuccess;
 }
