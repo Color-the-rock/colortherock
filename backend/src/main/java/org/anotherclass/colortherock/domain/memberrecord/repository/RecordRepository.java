27aca25222501617820d6c1543e44c769d0a3e61
johan <johankkong@gmail.com>
2023-01-25 13:07:09 +0900

    :bug: fix : memberRecord repository and S3Service bug fix

--- backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/repository/RecordRepository.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/repository/RecordRepository.java
@@ -1,10 +1,9 @@
 package org.anotherclass.colortherock.domain.memberrecord.repository;
 
 import org.anotherclass.colortherock.domain.member.entity.Member;
-import org.anotherclass.colortherock.domain.video.entity.Video;
+import org.anotherclass.colortherock.domain.memberrecord.entity.MemberRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecordRepository extends JpaRepository<Video, Long> {
-    Long countByMemberAndLevel(Member member, int level);
-    Long countByMemberAndLevelAndIsSuccessIsTrue(Member member, int level);
+public interface RecordRepository extends JpaRepository<MemberRecord, Long> {
+    MemberRecord findByMember(Member member);
 }
--- backend/src/main/java/org/anotherclass/colortherock/domain/video/service/S3Service.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/video/service/S3Service.java
@@ -7,9 +7,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.Level;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 
-@Log4j2
 @Service
 public class S3Service {
     private AmazonS3 s3Client;
@@ -42,15 +38,11 @@ public class S3Service {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(this.region)
                 .build();
-
-        log.log(Level.INFO, "s3Client 생성완료");
     }
 
-    public String upload(MultipartFile file) throws IOException {
-        // 중복 방지를 위해 기존 파일명앞에 시간을 추가한다.
-        String fileName = DateTime.now() + file.getOriginalFilename();
-        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+    public String upload(MultipartFile file, String videoName) throws IOException {
+        s3Client.putObject(new PutObjectRequest(bucket, videoName, file.getInputStream(), null)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
-        return s3Client.getUrl(bucket, fileName).toString();
+        return s3Client.getUrl(bucket, videoName).toString();
     }
 }
