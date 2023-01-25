e9c925c366f3cfe4a0f1da3dfe264a01b4477782
송현주 <songthdo427@gmail.com>
2023-01-25 09:32:36 +0900

    :lipstick: style :
    add mypage & style

---
+++ frontend/color-the-rock-app/src/pages/MyPage/style.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
+  padding: 0px 16px;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 2.125rem;
+  letter-spacing: -0.01em;
+  padding-bottom: 0.4rem;
+  border-bottom: 1px solid #272727;
+`;
+
+export const Nickname = styled.label``;
+export const Text = styled.label``;
+export const Button = styled(Link)``;
