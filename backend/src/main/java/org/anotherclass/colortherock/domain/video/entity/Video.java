837a3ac8781e1c7ba075ec9e3c548cfbee7d3d60
johan kong <johankkong@gmail.com>
2023-01-26 15:47:27 +0900

    🐛 fix : delete @Setter annotation
    관련 메소드 수정.

--- backend/src/main/java/org/anotherclass/colortherock/domain/video/entity/Video.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/video/entity/Video.java
@@ -50,7 +50,7 @@ public class Video {
     private VideoBoard videoBoard;
 
     @Builder
-    public Video(LocalDate shootingDate, Integer level, String gymName, String s3URL, Boolean isSuccess, String thumbnailURL, String color) {
+    public Video(LocalDate shootingDate, Integer level, String gymName, String s3URL, Boolean isSuccess, String thumbnailURL, String color, Member member, String videoName) {
         this.shootingDate = shootingDate;
         this.level = level;
         this.gymName = gymName;
@@ -58,6 +58,8 @@ public class Video {
         this.isSuccess = isSuccess;
         this.thumbnailURL= thumbnailURL;
         this.color = color;
+        this.member = member;
+        this.videoName = videoName;
     }
 
 }
