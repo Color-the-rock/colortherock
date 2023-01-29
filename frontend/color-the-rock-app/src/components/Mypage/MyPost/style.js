95250a32d8834ef5a3257a34a8d693979329772b
송현주 <songthdo427@gmail.com>
2023-01-29 19:33:55 +0900

    :lipstick: style :
    update mypage style

--- frontend/color-the-rock-app/src/components/Mypage/MyPost/style.js
+++ frontend/color-the-rock-app/src/components/Mypage/MyPost/style.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const VideoList = styled.div`
+  max-width: 600px;
   display: grid;
   gap: 1rem;
   align-self: center;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   @media (max-width: 1070px) {
     grid-template-columns: 1fr 1fr 1fr;
   }
