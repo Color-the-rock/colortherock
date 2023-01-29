421a132730d0a02ebc1a23f1c69c6e9090d4b66c
송현주 <songthdo427@gmail.com>
2023-01-29 20:06:52 +0900

    :sparkles: feat :
    add video component * settings

--- frontend/color-the-rock-app/src/components/Mypage/MyPost/index.jsx
+++ frontend/color-the-rock-app/src/components/Mypage/MyPost/index.jsx
@@ -1,39 +1,67 @@
 import React from "react";
 import Video from "../Video";
 import * as S from "./style";
+
+const videos = [
+  {
+    sources: [
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    ],
+    thumb:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDH3NZZuW6DeOsQGNKBvtRiNULwHKaeLhtw&usqp=CAU",
+    title: "Big Buck Bunny",
+    color: "빨강",
+  },
+  {
+    sources: [
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    ],
+    thumb:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDH3NZZuW6DeOsQGNKBvtRiNULwHKaeLhtw&usqp=CAU",
+    title: "Elephant Dream",
+    color: "노랑",
+  },
+  {
+    sources: [
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    ],
+    thumb:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDH3NZZuW6DeOsQGNKBvtRiNULwHKaeLhtw&usqp=CAU",
+    title: "For Bigger Blazes",
+    color: "초록",
+  },
+  {
+    sources: [
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    ],
+    thumb:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDH3NZZuW6DeOsQGNKBvtRiNULwHKaeLhtw&usqp=CAU",
+    title: "For Bigger Escape",
+    color: "보라",
+  },
+  {
+    sources: [
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+    ],
+    thumb:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDH3NZZuW6DeOsQGNKBvtRiNULwHKaeLhtw&usqp=CAU",
+    title: "For Bigger Fun",
+    color: "하늘",
+  },
+];
+
 const MyPost = () => {
-  const data = [
-    {
-      id: 1,
-      title: "제목입니다!1",
-      color: "빨강",
-    },
-    {
-      id: 2,
-      title: "제목입니다!3",
-      color: "파랑",
-    },
-    {
-      id: 3,
-      title: "제목입니다!4",
-      color: "노랑",
-    },
-    {
-      id: 4,
-      title: "제목입니다!4",
-      color: "초록",
-    },
-    {
-      id: 5,
-      title: "제목입니다!5",
-      color: "보라",
-    },
-  ];
   return (
     <S.VideoList>
-      {data &&
-        data.map((item) => (
-          <Video key={item.id} title={item.title} color={item.color} />
+      {videos &&
+        videos.map((item) => (
+          <Video
+            key={item.sources}
+            title={item.title}
+            color={item.color}
+            thumb={item.thumb}
+            sources={item.sources}
+          />
         ))}
     </S.VideoList>
   );
--- frontend/color-the-rock-app/src/components/Mypage/Video/index.jsx
+++ frontend/color-the-rock-app/src/components/Mypage/Video/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import * as S from "./style";
-import TestImg from "../../../assets/img/intro/bg-intro.png";
-const Video = ({ title, color }) => {
+const Video = ({ thumb, title, color, sources }) => {
+  console.log(thumb);
   return (
-    <S.Container>
-      <S.ThumbnailImg src={TestImg} alt="썸네일 이미지" />
-      <S.Text>{title}</S.Text>
-      <S.Tag>{color}</S.Tag>
+    <S.Container poster={thumb} controls>
+      <source src={sources[0]} type="video/mp4" />
     </S.Container>
+    //  <S.ThumbnailImg src={thumb} alt="썸네일 이미지" />
+    //  <S.Text>{title}</S.Text>
+    //  <S.Tag>{color}</S.Tag>
   );
 };
 
