be9a9f18ee3b0145d35fc2fa29eae8cce7e47c03
yezi <yejixheo@gmail.com>
2023-01-30 16:44:40 +0900

    🎨 style : rename error code

--- backend/src/main/java/org/anotherclass/colortherock/global/error/GlobalErrorCode.java
+++ backend/src/main/java/org/anotherclass/colortherock/global/error/GlobalErrorCode.java
@@ -21,11 +21,11 @@ public enum GlobalErrorCode {
     // 영상과 관련된 Exception
     VIDEO_NOT_FOUND(404, "V100", "해당 id에 해당하는 영상이 없습니다."),
     NOT_VIDEO_OWNER(400, "V200", "영상의 주인과 요청한 멤버가 다릅니다."),
-    NO_SUCH_USER(404, "G400", "유저 정보를 찾을 수 없음"),
-    NO_SUCH_POST(404, "VB100", "해당하는 완등 영상 글을 찾을 수 없습니다."),
-    WRITER_MISMATCH(403, "VB200", "사용자와 작성자가 일치하지 않습니다."),
-    NO_SUCH_COMMENT(404, "C100", "해당하는 댓글을 찾을 수 없습니다."),
-   // 라이브와 관련된 Exception
+    USER_NOT_FOUND(404, "G400", "유저 정보를 찾을 수 없음"),
+    POST_NOT_FOUND(404, "VB100", "해당하는 완등 영상 글을 찾을 수 없습니다."),
+    NOT_WRITER(403, "VB200", "사용자와 작성자가 일치하지 않습니다."),
+    COMMENT_NOT_FOUND(404, "C100", "해당하는 댓글을 찾을 수 없습니다."),
+    // 라이브와 관련된 Exception
     SESSION_NOT_FOUND(400, "L100", "세션을 찾을 수 없습니다."),
     RECORDING_START_BAD_REQUEST(400, "L101", "잘못된 녹화 요청입니다"),
     // 기록과 관련된 Exception
