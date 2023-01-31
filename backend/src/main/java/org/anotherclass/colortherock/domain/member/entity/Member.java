0232bfb4a0d0a6e71612be01e2073dbaadd7219d
Tae gyu Choi <suker800@gmail.com>
2023-01-31 13:50:26 +0900

    :recycle: refactoring member, video set @GeneratedValue(strategy = GenerationType.IDENTITY)

--- backend/src/main/java/org/anotherclass/colortherock/domain/member/entity/Member.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/member/entity/Member.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Table(name = "member")
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
