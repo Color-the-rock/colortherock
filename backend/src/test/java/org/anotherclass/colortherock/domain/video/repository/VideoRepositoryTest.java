2c7a4c0c80288bf7080afc6dae29e118036f21e9
johan kong <johankkong@gmail.com>
2023-02-02 01:52:29 +0900

    ✅ test : getting calendar color feature test
    repository에선 날짜와 레벨을 조회. controller에선 각 날짜에 대한 색상을 반환.

--- backend/src/test/java/org/anotherclass/colortherock/domain/video/repository/VideoRepositoryTest.java
+++ backend/src/test/java/org/anotherclass/colortherock/domain/video/repository/VideoRepositoryTest.java
@@ -4,8 +4,10 @@ package org.anotherclass.colortherock.domain.video.repository;
 import org.anotherclass.colortherock.domain.member.entity.Member;
 import org.anotherclass.colortherock.domain.member.repository.MemberRepository;
 import org.anotherclass.colortherock.domain.memberrecord.response.VisitListResponse;
+import org.anotherclass.colortherock.domain.video.dto.DateLevelDto;
 import org.anotherclass.colortherock.domain.video.entity.Video;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +24,12 @@ public class VideoRepositoryTest {
     private MemberRepository memberRepository;
     @Autowired
     private VideoRepository videoRepository;
-    @Test
-    @DisplayName("암장 방문 통계 반환")
-    public void 방문통계_반환_성공() {
+
+    private Member member;
+    @BeforeEach
+    public void setMemberAndVideos() {
         // given
-        Member member = Member.builder()
+        member = Member.builder()
                 .email("johan@rock.com")
                 .registrationId(Member.RegistrationId.kakao)
                 .nickname("nickname").build();
@@ -34,22 +37,41 @@ public class VideoRepositoryTest {
         for (int i = 0; i < 10; i++) {
             Video video = Video.builder()
                     .shootingDate(LocalDate.parse("2023-01-1" + i))
+                    .level(1)
                     .member(member)
+                    .isSuccess(true)
                     .gymName("tempGym1").build();
             videoRepository.save(video);
         }
         for (int i = 0; i < 10; i++) {
             Video video = Video.builder()
                     .shootingDate(LocalDate.parse("2023-01-1" + i))
+                    .level(2)
                     .member(member)
+                    .isSuccess(true)
                     .gymName("tempGym2").build();
             videoRepository.save(video);
         }
+    }
+    @Test
+    @DisplayName("암장 방문 통계 조회")
+    public void 방문통계_조회_성공() {
         // when
         List<VisitListResponse> visitList = videoReadRepository.searchVisitCount(member);
         // then
         Assertions.assertEquals(2, visitList.size());
         Assertions.assertEquals("tempGym1", visitList.get(0).getGymName());
         Assertions.assertEquals(10, visitList.get(1).getCount());
+    }
+
+    @Test
+    @DisplayName("날짜별 완등 레벨 조회")
+    public void 레벨_조회_성공() {
+        // when
+        List<DateLevelDto> dtos = videoReadRepository.searchDailyColor(member, LocalDate.parse("2023-01-01"), LocalDate.parse("2023-01-31"));
+        // then
+        Assertions.assertEquals(20, dtos.size());
+        Assertions.assertEquals("2023-01-10", dtos.get(0).getDate().toString());
+        Assertions.assertEquals(1, dtos.get(1).getLevel());
     }
 }
