f55a39e1a130980d296a1cc69ee7560e65324e9d
Tae gyu Choi <suker800@gmail.com>
2023-02-01 16:39:51 +0900

    :sparkles: swagger에서 토큰 입력할수있게 수정

--- backend/src/main/java/org/anotherclass/colortherock/global/swagger/SwaggerConfig.java
+++ backend/src/main/java/org/anotherclass/colortherock/global/swagger/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package org.anotherclass.colortherock.global.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import jdk.javadoc.doclet.Doclet;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +20,27 @@ public class SwaggerConfig {
                 .pathsToMatch("/**")
                 .build();
     }
+
     @Bean
     public OpenAPI ColorTheRockOpenAPI() {
+        String jwtSchemeName = "jwtAuth";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
         return new OpenAPI()
                 .info(new Info().title("Color The Rock API")
                         .description("Color The Rock API 명세서입니다.")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
+
+
 }
