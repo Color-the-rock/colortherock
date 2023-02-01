26cde2721eec1be14bfcb83a50d548ad48a0e4cc
shyunju7 <shyunju7@naver.com>
2023-02-01 17:00:36 +0900

    💄style : update CommentButton style & feat CommentButton onClick 설정

--- frontend/color-the-rock-app/src/components/Common/CommentBtn/style.js
+++ frontend/color-the-rock-app/src/components/Common/CommentBtn/style.js
@@ -1,5 +1,4 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 export const Container = styled.span`
   display: flex;
@@ -9,9 +8,9 @@ export const Container = styled.span`
   width: 100%;
   background: #272727;
   border-radius: 20px;
-  border: 1px solid var(--color-border);
-  /* box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7); */
-`
+  border: none;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+`;
 
 export const InputBtn = styled.input`
   background-color: transparent;
@@ -19,10 +18,10 @@ export const InputBtn = styled.input`
   width: 80%;
   padding-left: 1rem;
   color: white;
-`
+`;
 
 export const Ornament = styled.div`
   width: 20%;
   padding-right: 1rem;
   text-align: right;
-`
+`;
