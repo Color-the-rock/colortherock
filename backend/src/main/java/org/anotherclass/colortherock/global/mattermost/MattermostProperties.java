603e5194dca1ca20915f780ab1daf4dc1291bf1d
Tae gyu Choi <suker800@gmail.com>
2023-02-03 16:32:27 +0900

    ✨ feat : Mattermost 속성 값을 가진 빈 추가

---
+++ backend/src/main/java/org/anotherclass/colortherock/global/mattermost/MattermostProperties.java
@@ -0,0 +1,28 @@
+package org.anotherclass.colortherock.global.mattermost;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties("notification.mattermost")
+@Primary
+public class MattermostProperties {
+
+    private String channel;
+    private String pretext;
+    private String color = "#ff5d52";
+    private String authorName;
+    private String authorIcon;
+    private String title;
+    private String text = "";
+    private String footer = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+
+}
