9fe26b9819aff3ab5d00c4d3e82e0f3df5e9a659
Tae gyu Choi <suker800@gmail.com>
2023-02-06 17:40:43 +0900

    ♻️ refactor : 멤버 swagger 설명 추가

--- backend/src/main/java/org/anotherclass/colortherock/domain/member/request/DuplicateNicknameRequest.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/member/request/DuplicateNicknameRequest.java
@@ -1,5 +1,6 @@
 package org.anotherclass.colortherock.domain.member.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 public class DuplicateNicknameRequest {
     @Pattern(regexp = "^\\S*$", message = "닉네임에 공백이 있으면 안됩니다.")
-    @Pattern(regexp = "^[A-Za-z0-9가-힣]{2,16}$",message = "닉네임은 2글자 이상 16자 이하입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9가-힣]{2,16}$", message = "닉네임은 2글자 이상 16자 이하입니다.")
+    @Schema(description = "닉네임")
     String nickname;
 }
