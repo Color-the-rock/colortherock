c7ec67fdbc4ffdf83bacb0574ea8c7e2c5cf1145
Tae gyu Choi <suker800@gmail.com>
2023-02-07 17:36:47 +0900

    ♻️ refactor : videocomment api 요청,응답 description 추가

--- backend/src/main/java/org/anotherclass/colortherock/domain/videocomment/request/CommentUpdateRequest.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/videocomment/request/CommentUpdateRequest.java
@@ -1,20 +1,25 @@
 package org.anotherclass.colortherock.domain.videocomment.request;
 
-import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "댓글 수정 요청")
 public class CommentUpdateRequest {
 
     @NotNull
+    @Schema(description = "댓글 id")
     private Long commentId;
     @NotNull
+    @Schema(description = "댓글 내용")
     private String content;
 
 }
