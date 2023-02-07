8d94053fcecb8c12ff108e8b226214bccc686072
Tae gyu Choi <suker800@gmail.com>
2023-02-07 17:32:10 +0900

    ♻️ refactor : videoboard api 요청,응답 description 추가

--- backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/response/VideoBoardDetailResponse.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/videoboard/response/VideoBoardDetailResponse.java
@@ -1,5 +1,6 @@
 package org.anotherclass.colortherock.domain.videoboard.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -9,10 +10,15 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "영상 게시글 응답")
 public class VideoBoardDetailResponse {
+    @Schema(description = "영상 게시글 id")
     private Long videoBoardId;
+    @Schema(description = "닉네임")
     private String nickname;
+    @Schema(description = "제목")
     private String title;
+    @Schema(description = "글 쓴 시간")
     private LocalDateTime createdDate;
 
 }
