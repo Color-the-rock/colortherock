64f38f8c3395e892561b0db584be01e40ac72c47
Tae gyu Choi <suker800@gmail.com>
2023-02-07 17:00:11 +0900

    ♻️ refactor : response에 swagger 설명 추가

--- backend/src/main/java/org/anotherclass/colortherock/domain/live/response/PrevRecordingListResponse.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/live/response/PrevRecordingListResponse.java
@@ -1,25 +1,36 @@
 package org.anotherclass.colortherock.domain.live.response;
 
 import io.openvidu.java.client.Recording;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-public class RecordingListResponse {
+public class PrevRecordingListResponse {
+
+    @Schema(description = "녹화 영상 id")
     private String recordingId;
+    @Schema(description = "녹화 영상 제목")
     private String recordingName;
+
+    @Schema(description = "세션 id")
     private String sessionId;
+
+    @Schema(description = "녹화 경로 url")
     private String url;
+    @Schema(description = "생성 시간")
     private Long createdAt;
+
+    @Schema(description = "녹화 시간")
     private Double duration;
 
     @Builder
-    public RecordingListResponse(String recordingId, String recordingName, String sessionId, String url) {
+    public PrevRecordingListResponse(String recordingId, String recordingName, String sessionId, String url) {
         this.recordingId = recordingId;
         this.recordingName = recordingName;
         this.sessionId = sessionId;
         this.url = url;
     }
 
-    public RecordingListResponse(Recording recording) {
+    public PrevRecordingListResponse(Recording recording) {
         this.recordingId = recording.getId();
         this.recordingName = recording.getName();
         this.sessionId = recording.getSessionId();
