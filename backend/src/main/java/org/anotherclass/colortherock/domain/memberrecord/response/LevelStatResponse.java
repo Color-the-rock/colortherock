d0d20085f7002a0479846926182394b22f8426b6
Tae gyu Choi <suker800@gmail.com>
2023-02-07 17:12:20 +0900

    ♻️ refactor : 멤버 기록 api 응답 description 추가

--- backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/response/LevelStatResponse.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/memberrecord/response/LevelStatResponse.java
@@ -1,14 +1,19 @@
 package org.anotherclass.colortherock.domain.memberrecord.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter @Setter
+@Schema(description = "레벨별 통계 응답 객체")
 public class LevelStatResponse {
+    @Schema(description = "레벨")
     int level;
+    @Schema(description = "총합")
     long total;
+    @Schema(description = "성공 개수")
     long success;
 
     public LevelStatResponse(int level) {
