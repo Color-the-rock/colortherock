aad12453e935f728bbaea4975c3a80db525c9c14
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-02-10 13:01:31 +0900

    :zap: update: 세션에 유저정보 저장.
    .

--- frontend/color-the-rock-app/src/stores/reducers.js
+++ frontend/color-the-rock-app/src/stores/reducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage/session";
 import record from "./record/recordSlice";
 import streaming from "./streaming//streamingSlice";
 import users from "./users/userSlice";
