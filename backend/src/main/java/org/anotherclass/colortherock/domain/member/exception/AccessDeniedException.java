6089899f9fa850876844689f102aabc7e7ec54fe
Choi Tae gyu <suker800@gmail.com>
2023-02-13 20:00:02 +0900

    ♻️refactor : 사용하지 않는 메소드 삭제

--- backend/src/main/java/org/anotherclass/colortherock/domain/member/exception/AccessDeniedException.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/member/exception/AccessDeniedException.java
@@ -4,9 +4,6 @@ import org.anotherclass.colortherock.global.error.GlobalBaseException;
 import org.anotherclass.colortherock.global.error.GlobalErrorCode;
 
 public class AccessDeniedException extends GlobalBaseException {
-    public AccessDeniedException(String message, GlobalErrorCode errorCode) {
-        super(message, errorCode);
-    }
 
     public AccessDeniedException(GlobalErrorCode errorCode) {
         super(errorCode);
