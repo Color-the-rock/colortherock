f285332dd548c59d72f4e0221f10d797ed8b5484
Tae gyu Choi <suker800@gmail.com>
2023-02-13 15:05:28 +0900

    ♻️ refactor : 불필요한 method throws 삭제

--- backend/src/main/java/org/anotherclass/colortherock/domain/video/service/VideoService.java
+++ backend/src/main/java/org/anotherclass/colortherock/domain/video/service/VideoService.java
@@ -7,6 +7,7 @@ import org.anotherclass.colortherock.domain.member.exception.MemberNotFoundExcep
 import org.anotherclass.colortherock.domain.member.repository.MemberRepository;
 import org.anotherclass.colortherock.domain.memberrecord.exception.WrongMemberException;
 import org.anotherclass.colortherock.domain.memberrecord.response.VideoListResponse;
+import org.anotherclass.colortherock.domain.video.dto.DeletedVideoDto;
 import org.anotherclass.colortherock.domain.video.entity.Video;
 import org.anotherclass.colortherock.domain.video.exception.NotVideoExtensionException;
 import org.anotherclass.colortherock.domain.video.exception.VideoFileNameHasNotExtensionException;
@@ -15,17 +16,14 @@ import org.anotherclass.colortherock.domain.video.repository.VideoReadRepository
 import org.anotherclass.colortherock.domain.video.repository.VideoRepository;
 import org.anotherclass.colortherock.domain.video.request.MySuccessVideoRequest;
 import org.anotherclass.colortherock.domain.video.request.UploadVideoRequest;
-import org.anotherclass.colortherock.domain.video.dto.DeletedVideoDto;
 import org.anotherclass.colortherock.domain.videoboard.request.LocalSuccessVideoUploadRequest;
 import org.anotherclass.colortherock.global.error.GlobalErrorCode;
-import org.jcodec.api.JCodecException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,7 +40,7 @@ public class VideoService {
 
     // 로컬에서 영상게시판 통해 동영상 올리기
     @Transactional
-    public Long uploadSuccessVideo(MemberDetails memberDetails, MultipartFile newVideo, LocalSuccessVideoUploadRequest request) throws IOException, JCodecException {
+    public Long uploadSuccessVideo(MemberDetails memberDetails, MultipartFile newVideo, LocalSuccessVideoUploadRequest request) {
         Member member = memberRepository.findById(memberDetails.getMember().getId())
                 .orElseThrow(() -> new MemberNotFoundException(GlobalErrorCode.USER_NOT_FOUND));
         // S3 영상 저장 후 URL 얻어오기
@@ -52,12 +50,12 @@ public class VideoService {
         String thumbnailName = extractValidThumbName(member);
         String thumbnailURL = s3Service.uploadThumbnail(newVideo, thumbnailName);
         // request와 URL, name 을 DB에 저장
-        return saveSuccessVideo(member, videoName, s3URL,thumbnailName, thumbnailURL, request);
+        return saveSuccessVideo(member, videoName, s3URL, thumbnailName, thumbnailURL, request);
     }
 
     // 마이페이지에서 동영상 올리기
     @Transactional
-    public void uploadMyVideo(MemberDetails memberDetails, MultipartFile newVideo, UploadVideoRequest request) throws IOException, JCodecException {
+    public void uploadMyVideo(MemberDetails memberDetails, MultipartFile newVideo, UploadVideoRequest request) {
         Member member = memberRepository.findById(memberDetails.getMember().getId())
                 .orElseThrow(() -> new MemberNotFoundException(GlobalErrorCode.USER_NOT_FOUND));
         // S3 영상 저장 후 URL 얻어오기
