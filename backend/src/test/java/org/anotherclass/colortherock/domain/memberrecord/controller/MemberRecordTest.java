cfb8b8f5257ae60f5f26f0e731ec5d1262d1f0bb
Choi Tae gyu <suker800@gmail.com>
2023-02-13 20:42:52 +0900

    ♻️refactor : 프로젝트 import 최적화

--- backend/src/test/java/org/anotherclass/colortherock/domain/memberrecord/controller/MemberRecordTest.java
+++ backend/src/test/java/org/anotherclass/colortherock/domain/memberrecord/controller/MemberRecordTest.java
@@ -34,6 +34,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class MemberRecordTest extends IntegrationTest {
 
     @Autowired
