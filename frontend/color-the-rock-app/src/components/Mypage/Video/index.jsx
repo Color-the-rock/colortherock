de3d507a4023486f0cb6d6add25dbb0b0a679c2f
송현주 <songthdo427@gmail.com>
2023-02-13 19:02:31 +0900

    💄 style : 마이페이지 비디오 컴포넌트 UI 수정

--- frontend/color-the-rock-app/src/components/Mypage/Video/index.jsx
+++ frontend/color-the-rock-app/src/components/Mypage/Video/index.jsx
@@ -5,13 +5,12 @@ import * as S from "./style";
 const Video = ({ id, thumbnailURL, gymName, level, color }) => {
   const navigate = useNavigate();
   const handleOnClickThumbNail = () => {
-    console.log("handleOnClickThumbNail()... ");
     navigate(`/preview?videoId=${id}`);
   };
   return (
     <S.Container onClick={handleOnClickThumbNail}>
       <S.ThumbnailImg src={thumbnailURL} alt="썸네일 이미지" />
-      <S.Tag>{gymName}</S.Tag>
+      <S.Text>{gymName}</S.Text>
       <S.Tag>{color}</S.Tag>
       <S.Tag>{level}</S.Tag>
     </S.Container>
