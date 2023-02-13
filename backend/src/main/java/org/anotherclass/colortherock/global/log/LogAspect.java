cd0782c80d59f302922e5b933f7458000bf8051a
Tae gyu Choi <suker800@gmail.com>
2023-02-13 10:42:03 +0900

    ♻️ refactor : log 패키지에 있는 클래스 javadoc 추가

--- backend/src/main/java/org/anotherclass/colortherock/global/log/LogAspect.java
+++ backend/src/main/java/org/anotherclass/colortherock/global/log/LogAspect.java
@@ -10,12 +10,21 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
+/**
+ * 실행 시간을 측정하는 Aspect
+ *
+ */
 @Aspect
 @Slf4j
 @Component
 @Order(1)
 public class LogAspect {
 
+    /**
+     * 조인 포인트 주변에 시간 측정 코드를 넣어서 메소드가 수행되는 시간을 측정한다.
+     * @param proceedingJoinPoint 적용된 조인포인트
+     * @return 원래 결과
+     */
     @Around("execution(* org.anotherclass.colortherock.domain..*(..)))")
     public Object logMethodExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
