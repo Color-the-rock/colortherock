8a339504f0803864acca389aa5e1c39a9b8b820d
송현주 <songthdo427@gmail.com>
2023-02-14 17:54:02 +0900

    💄 style : preview button 위치 조정

--- frontend/color-the-rock-app/src/pages/Preview/style.js
+++ frontend/color-the-rock-app/src/pages/Preview/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
   width: 100%;
   height: calc(var(--vh, 1vh) * 100);
   background-color: transparent;
@@ -30,6 +31,8 @@ export const Button = styled.button`
 `;
 
 export const Wrapper = styled.div`
+  position: absolute;
+  top: 32px;
   display: flex;
   width: 100%;
   padding: 0 1rem;
