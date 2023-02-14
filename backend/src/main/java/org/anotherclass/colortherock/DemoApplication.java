c3512215edb092269bddffea28e6c219a1447674
yezi <yejixheo@gmail.com>
2023-02-14 15:27:46 +0900

    🐛 fix : fix time zone to asia
    - RDS 저장시 타임존이 AWS 기준으로 설정되는 것을 변경했습니다.

--- backend/src/main/java/org/anotherclass/colortherock/DemoApplication.java
+++ backend/src/main/java/org/anotherclass/colortherock/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @OpenAPIDefinition( servers = { @Server(url = "/", description = "Default Server url") } )
 @SpringBootApplication
 @EnableJpaAuditing
@@ -13,6 +16,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
